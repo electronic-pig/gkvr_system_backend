d31ac9dafab941920856c38a9240192903dfd3f9
zzc <2278860740@qq.com>
2023-07-15 16:56:03 +0800

    zhouzichuan 2023/7/15 新增查询所有985学校的功能

--- src/main/java/com/scu/gkvr_system/controller/SchoolInfoController.java
+++ src/main/java/com/scu/gkvr_system/controller/SchoolInfoController.java
@@ -36,4 +36,14 @@ public class SchoolInfoController {
         }
         return Result.fail(20001, "数据为空");
     }
+
+    @GetMapping("/985")
+    public Result<Map<String, Object>> get985Schools() {
+        schoolInfoService.getAllSchool();//获取全部学校信息
+        Map<String, Object> data = schoolInfoService.get985Schools();
+        if (data != null) {
+            return Result.success("985类学校信息查询成功", data);
+        }
+        return Result.fail(20001, "数据为空");
+    }
 }
