67439e18b97e3fbdde933c504b8c885aa99778ff
weeadd <943941375@qq.com>
2023-07-16 08:49:33 +0800

    wj 2023/7/15 补全MajorInfoController

--- src/main/java/com/scu/gkvr_system/controller/MajorInfoController.java
+++ src/main/java/com/scu/gkvr_system/controller/MajorInfoController.java
@@ -1,8 +1,15 @@
 package com.scu.gkvr_system.controller;
 
+import com.scu.common.vo.Result;
+import com.scu.gkvr_system.service.ISchoolInfoService;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.annotation.Resource;
+import java.util.Map;
 
 /**
  * <p>
@@ -16,5 +23,16 @@ import org.springframework.stereotype.Controller;
 @RequestMapping("/majorInfo")
 @CrossOrigin
 public class MajorInfoController {
+    @Resource
+    private ISchoolInfoService majorInfoService;
+    @GetMapping
+    public Result<Map<String, Object>> getSchoolsByPage(@RequestParam("page") int page) {
+        majorInfoService.getAllSchool();//获取全部学校信息
+        Map<String, Object> data = majorInfoService.getSchoolsByPage(page);
+        if (data != null) {
+            return Result.success("专业信息查询成功", data);
+        }
+        return Result.fail(20001, "数据为空");
+    }
 
 }
