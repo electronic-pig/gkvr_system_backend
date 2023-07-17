c212112f29320f2680c4867a276e37224c92e38a
zzc <2278860740@qq.com>
2023-07-17 09:44:37 +0800

    zhouzichuan 2023/7/17 修改了学校详情功能,现在会返回学校信息和这个学校的所有专业信息和四川理科近三年分数信息;增加了用户志愿类

---
+++ src/main/java/com/scu/gkvr_system/mapper/UserVoluntaryMapper.java
@@ -0,0 +1,16 @@
+package com.scu.gkvr_system.mapper;
+
+import com.scu.gkvr_system.entity.UserVoluntary;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author zzc
+ * @since 2023-07-17
+ */
+public interface UserVoluntaryMapper extends BaseMapper<UserVoluntary> {
+
+}
---
+++ src/main/java/com/scu/gkvr_system/service/impl/UserVoluntaryServiceImpl.java
@@ -0,0 +1,20 @@
+package com.scu.gkvr_system.service.impl;
+
+import com.scu.gkvr_system.entity.UserVoluntary;
+import com.scu.gkvr_system.mapper.UserVoluntaryMapper;
+import com.scu.gkvr_system.service.IUserVoluntaryService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author zzc
+ * @since 2023-07-17
+ */
+@Service
+public class UserVoluntaryServiceImpl extends ServiceImpl<UserVoluntaryMapper, UserVoluntary> implements IUserVoluntaryService {
+
+}
