14eb5b60b1f2ad62fc3d1c9a3b4d2a9e932facee
zzc <2278860740@qq.com>
2023-07-17 09:23:57 +0800

    zhouzichuan 2023/7/17 修改了学校详情功能,现在会返回学校信息和这个学校的所有专业信息

---
+++ src/main/java/com/scu/gkvr_system/mapper/MajorScoreMapper.java
@@ -0,0 +1,16 @@
+package com.scu.gkvr_system.mapper;
+
+import com.scu.gkvr_system.entity.MajorScore;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author zzc
+ * @since 2023-07-17
+ */
+public interface MajorScoreMapper extends BaseMapper<MajorScore> {
+
+}
