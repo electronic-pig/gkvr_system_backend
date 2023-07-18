61a407bcd89718d528f7d76b0c80965319131dd9
zzc <2278860740@qq.com>
2023-07-18 21:29:18 +0800

    zhouzichuan 2023/7/18 修复了bug

--- src/main/java/com/scu/gkvr_system/service/impl/SchoolInfoServiceImpl.java
+++ src/main/java/com/scu/gkvr_system/service/impl/SchoolInfoServiceImpl.java
@@ -145,7 +145,7 @@ public class SchoolInfoServiceImpl extends ServiceImpl<SchoolInfoMapper, SchoolI
         System.out.println(endIndex);
         List<SchoolInfoAndScore> list = new ArrayList<>();
         int count = endIndex-startIndex;
-        for (int i = startIndex;i<Math.min(count+endIndex,schools.size());i++) {
+        for (int i = startIndex;i<Math.min(count+endIndex-10,schools.size());i++) {
             SchoolInfoAndScore schoolInfoAndScore = new SchoolInfoAndScore();
             SchoolInfo schoolInfo = schools.get(i);
             LambdaQueryWrapper<ScLiScoreNew> wrapper = new LambdaQueryWrapper<>();
@@ -156,8 +156,7 @@ public class SchoolInfoServiceImpl extends ServiceImpl<SchoolInfoMapper, SchoolI
                 continue;
             }
 
-//            System.out.println(scLiScoreNew.get(0));
-//            System.out.println(scLiScoreNew.size());
+
             schoolInfoAndScore.setId(schoolInfo.getId());
             schoolInfoAndScore.setBelongs(schoolInfo.getBelongs());
             schoolInfoAndScore.setCentral(schoolInfo.getCentral());
@@ -191,7 +190,6 @@ public class SchoolInfoServiceImpl extends ServiceImpl<SchoolInfoMapper, SchoolI
         }
 
 
-//        List<SchoolInfo> subList = schools.subList(startIndex, endIndex);
 
         Map<String, Object> result = new HashMap<>();
         result.put("page", page);
