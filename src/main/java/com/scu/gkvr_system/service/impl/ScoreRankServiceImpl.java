ac8bac5b92b4962c91bcd68e442fa150cbc903f4
weeadd <943941375@qq.com>
2023-07-19 09:30:20 +0800

    wj 2023/7/19 新增一分一段表，reco页改为传参分数获取真实位次来推荐学校

---
+++ src/main/java/com/scu/gkvr_system/service/impl/ScoreRankServiceImpl.java
@@ -0,0 +1,20 @@
+package com.scu.gkvr_system.service.impl;
+
+import com.scu.gkvr_system.entity.ScoreRank;
+import com.scu.gkvr_system.mapper.ScoreRankMapper;
+import com.scu.gkvr_system.service.IScoreRankService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author wj
+ * @since 2023-07-19
+ */
+@Service
+public class ScoreRankServiceImpl extends ServiceImpl<ScoreRankMapper, ScoreRank> implements IScoreRankService {
+
+}
