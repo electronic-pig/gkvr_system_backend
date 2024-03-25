e89e3d95d30d90c3660573657bdd2b1f837a6913
liyang <2234333815@qq.com>
2024-03-25 12:52:20 +0800

    处理跨域

--- src/main/java/com/scu/gkvr_system_backend/controller/ScLiScoreController.java
+++ src/main/java/com/scu/gkvr_system_backend/controller/ScLiScoreController.java
@@ -3,10 +3,7 @@ package com.scu.gkvr_system_backend.controller;
 import com.scu.gkvr_system_backend.service.ScLiScoreService;
 import com.scu.gkvr_system_backend.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -20,6 +17,7 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping("/scLiScore")
+@CrossOrigin
 public class ScLiScoreController {
 
     @Autowired
